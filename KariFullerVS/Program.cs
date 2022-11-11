d15c0f9bcda2ddd9d706eb306c994edac8f84202
Kari Fuller <38434486+karicfuller@users.noreply.github.com>
2022-11-11 14:51:14 -0800

    Student Daily Report Assignment

--- KariFullerVS/Program.cs
+++ KariFullerVS/Program.cs
@@ -1,26 +1,42 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace KariFullerVS
+namespace TTA_Student_Daily_Report
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            Console.WriteLine("The Tech Academy"); 
+            Console.WriteLine("TTA Student Daily Report");
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string yourCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            string whatPageNum = Console.ReadLine();
+            int pageNum = Convert.ToInt32(whatPageNum); //converts string to int
+
+            Console.WriteLine("Do you need help with anything? Please answer True of False?");
+            string needHelp = Console.ReadLine();
+            bool help = Convert.ToBoolean(needHelp); //converts string to boolean
+
+            Console.WriteLine("Were there any positive experiences you'd like to provide? Please give specifics.");
+            string posExperience = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string studyHours = Console.ReadLine();
+            int hoursOfStudy = Convert.ToInt32(studyHours); //converts string to int
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+
+            Console.ReadLine(); //keeps window open
+        }
     }
 }
