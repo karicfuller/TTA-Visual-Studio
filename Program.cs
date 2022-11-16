1b6ad30bc37367f736bf0ac3d04e4fc05b3309ea
Kari Fuller <38434486+karicfuller@users.noreply.github.com>
2022-11-15 17:13:18 -0800

    Math and Comparison Operator Assignment - Final

--- Program.cs
+++ Program.cs
@@ -10,8 +10,9 @@ namespace Math_and_Comparison_Operator_Assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anonymous Income Comaparison Program");
+            Console.WriteLine("Anonymous Income Comaparison Program");  //title
 
+            //Person 1 info
             Console.WriteLine("Person 1");
 
             Console.WriteLine("What is your hourly rate?");
@@ -23,8 +24,9 @@ namespace Math_and_Comparison_Operator_Assignment
             double hoursPerWeek = Convert.ToDouble(numOfHours);
 
             Console.WriteLine("Annual Salary of Person 1");
-            Console.WriteLine(rateHourly * hoursPerWeek * 52);
+            Console.WriteLine(rateHourly * hoursPerWeek * 52); //hourly pay*hours worked a week*52
 
+            //Person 2 info
             Console.WriteLine("Person 2");
 
             Console.WriteLine("What is your hourly rate?");
@@ -36,9 +38,9 @@ namespace Math_and_Comparison_Operator_Assignment
             double hoursOfWork = Convert.ToDouble(amountOfHours);
 
             Console.WriteLine("Annual Salary of Person 2");
-            Console.WriteLine(hourlyAmount * hoursOfWork * 52);
+            Console.WriteLine(hourlyAmount * hoursOfWork * 52); //hourly pay*hours worked a week*52
 
-            Console.ReadLine();
+            Console.ReadLine();  //keep open
 
             
 
