460d39f31133949601b0855436f57cddfad88cd4
Kari Fuller <38434486+karicfuller@users.noreply.github.com>
2022-11-16 13:12:30 -0800

    Boolean Logic Assignment

--- Program.cs
+++ Program.cs
@@ -4,55 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Math_and_Comparison_Operator_Assignment
+namespace BooleanLogicAssignment
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anonymous Income Comaparison Program");  //title
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            int age = Convert.ToInt32(yourAge); //convert string to int
 
-            //Person 1 info
-            Console.WriteLine("Person 1");
+            Console.WriteLine("You have a DUI? True or False?");
+            string haveDUI = Console.ReadLine();
+            bool dui = Convert.ToBoolean(haveDUI);  //convert string to bool
 
-            Console.WriteLine("What is your hourly rate?");
-            string hourlyRate = Console.ReadLine();
-            double rateHourly = Convert.ToDouble(hourlyRate);
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedTickets = Console.ReadLine();
+            int haveTickets = Convert.ToInt32(speedTickets);  //conver string to int
 
-            Console.WriteLine("How many hours do you work a week?");
-            string numOfHours = Console.ReadLine();
-            double hoursPerWeek = Convert.ToDouble(numOfHours);
-
-            Console.WriteLine("Annual Salary of Person 1");
-            double total1 = rateHourly * hoursPerWeek * 52; //hourly pay*hours worked a week*52
-            Console.WriteLine(total1);
-
-            //Person 2 info
-            Console.WriteLine("Person 2");
-
-            Console.WriteLine("What is your hourly rate?");
-            string ratePerHour = Console.ReadLine();
-            double hourlyAmount = Convert.ToDouble(ratePerHour);
-
-            Console.WriteLine("How many hours do you work a week?");
-            string amountOfHours = Console.ReadLine();
-            double hoursOfWork = Convert.ToDouble(amountOfHours);
-
-            Console.WriteLine("Annual Salary of Person 2");
-            double total2 = hourlyAmount * hoursOfWork * 52; //hourly pay*hours worked a week*52
-            Console.WriteLine(total2);
-
-            Console.WriteLine("Does Person 1 make more money than Person 2? True or False");
-            bool theMost = total1 > total2;
-            Console.WriteLine(theMost);
-
-            Console.ReadLine();  //keep open
-
-            
-
-
-
-
+            bool finding = age > 15 && dui == false && haveTickets < 3; //equation to find yes or no
+            Console.WriteLine("Are you qualified? " + finding);
+            Console.ReadLine();
         }
     }
 }
