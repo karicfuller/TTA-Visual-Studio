06fa14612e850cc545d5484d7900a1bfd1cc3c38
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-03 13:19:03 -0800

    Methods Assignment Start

---
+++ Methods Assignment/Methods Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select a number.");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
