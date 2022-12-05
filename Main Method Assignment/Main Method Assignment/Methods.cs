9d9b53002414b2b1107ebffa89f6111816d9934b
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-05 14:24:49 -0800

    Main Method Assignment - Complete/Needs review

---
+++ Main Method Assignment/Main Method Assignment/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Methods
+    {
+        public int Addition(int i)
+        {
+            int sum = i + 1; //will add 1 to int
+            return (sum);
+        }
+        public int Addition(decimal j) //using decimal
+        {
+            int theDecimal = Convert.ToInt32(j) + 1; //converting decimal to int
+            return (theDecimal);
+        }
+        public int Addition(string x)
+        {
+            int toInt = Convert.ToInt32(x) - 1; //changing string to int
+            return toInt;
+        }
+    }
+}
