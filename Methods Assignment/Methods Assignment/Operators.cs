d11568db9e6fd59c0b820d8e16714b5bea255481
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-05 12:51:27 -0800

    Methods Assignment - Completed/Needs Review

--- Methods Assignment/Methods Assignment/Operators.cs
+++ Methods Assignment/Methods Assignment/Operators.cs
@@ -11,14 +11,17 @@ namespace Methods_Assignment
         public int Addition(int i)
         {
             int sum = i + 1;
+            return (sum);
         }
         public int Subtract(int i)
         {
             int subtract = i - 1;
+            return (subtract);
         }
         public int Mulitply(int i)
         {
             int multiply = i * 1;
+            return (multiply);
         }
     }
 }
--- Methods Assignment/Methods Assignment/Program.cs
+++ Methods Assignment/Methods Assignment/Program.cs
@@ -12,6 +12,17 @@ namespace Methods_Assignment
         {
             Console.WriteLine("Select a number.");
             int inputNumber = Convert.ToInt32(Console.ReadLine());
+            Operators aClass = new Operators(); //Operators is name of class
+
+            int theNumber = aClass.Addition(inputNumber);
+            int nextNumber = aClass.Subtract(inputNumber);
+            int finalNumber = aClass.Mulitply(inputNumber);
+            Console.WriteLine(theNumber);
+            Console.WriteLine(nextNumber);
+            Console.WriteLine(finalNumber);
+
+            Console.ReadLine();
         }
+
     }
 }
