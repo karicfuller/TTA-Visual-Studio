99b32fdbef25b0675f6f2350e0e6e800de7ebd06
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-06 11:40:39 -0800

    Method Class Assignment/Complete-Needs Review

---
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _2_Integers ints = new _2_Integers(); //creating object
+
+            ints.twoInts(3, 5); //name of object.name of method = call a method
+            
+            Console.ReadLine();
+        }
+    }
+}
