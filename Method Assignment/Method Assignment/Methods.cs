12c14b9195d6d7ca682c9750f0015150502694cf
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-05 19:10:16 -0800

    Method Assignment - Complete/Needs Review

---
+++ Method Assignment/Method Assignment/Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Methods
+    {
+        public int twoInt(int i, int j = 5)
+        {
+            int firstNum = i + 1 + j;
+            return (firstNum);
+        }
+    }
+}
---
+++ Method Assignment/Method Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter one or two numbers, one at a time.");
+            int inputNums = Convert.ToInt32(Console.ReadLine());
+            
+            Methods method = new Methods();
+
+            try
+            {
+                int anotherNum = Convert.ToInt32(Console.ReadLine());
+                int number1 = method.twoInt(inputNums, anotherNum);
+                Console.WriteLine(number1);
+            }
+            catch
+            {
+                int number2 = method.twoInt(inputNums);
+                Console.WriteLine(number2);
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
