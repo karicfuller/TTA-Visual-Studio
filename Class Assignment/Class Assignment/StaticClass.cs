8ad1fa38643a00fa390c599de25783dd5990d3d5
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-07 12:46:35 -0800

    Complete/Needs Review

---
+++ Class Assignment/Class Assignment/StaticClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignment
+{
+    static class StaticClass
+    {
+        public static void VoidMethod()
+        {
+            Console.WriteLine("This is some text");
+        }
+    }
+}
