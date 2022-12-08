02d9ac5fe298f7ebabaaa6fece0cd27581c24019
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-08 09:08:24 -0800

    Complete/Needs Review

---
+++ Abstract Class Assignment/Abstract Class Assignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    class Employee : Person //Employee class inherits from the Person class
+    {
+        public override void SayName()
+        {
+        }
+    }
+}
---
+++ Abstract Class Assignment/Abstract Class Assignment/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; } //first property
+        public string lastName { get; set; } //second property
+
+        public abstract void SayName(); //method
+    }
+}
