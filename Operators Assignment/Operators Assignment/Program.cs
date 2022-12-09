e530ca5957f02b548764b91f9b39c75c83211e56
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-09 09:56:15 -0800

    Completed/Needs Review

---
+++ Operators Assignment/Operators Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Employee employee2 = new Employee();
+            employee.Id = 2;
+            employee2.Id = 3;
+            Console.WriteLine(employee == employee2);
+            Console.ReadLine();
+        }
+    }
+}
