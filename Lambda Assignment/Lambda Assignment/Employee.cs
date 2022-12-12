51b9e32bb08dbfed3640cd7df4e4e5a11f655a5e
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-12 04:21:55 -0800

    Unfinished/Need Help

---
+++ Lambda Assignment/Lambda Assignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Assignment
+{
+    class Employee
+    {
+        public int Id { get; set; } //property 
+        public string FirstName { get; set; } //property
+        public string LastName { get; set; } //property
+    }
+}
