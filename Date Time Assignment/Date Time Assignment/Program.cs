e0b55f1606278b31d3d0b8386a344137fb92d2b1
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-14 04:59:59 -0800

    Incomplete/Needs Review

---
+++ Date Time Assignment/Date Time Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Date_Time_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime();
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Pick a number");
+            int inputTime = Convert.ToInt32(Console.ReadLine());
+            DateTime newDateTime = dateTime.AddHours(inputTime);
+            Console.WriteLine(newDateTime);
+
+            Console.ReadLine();
+        }
+    }
+}
