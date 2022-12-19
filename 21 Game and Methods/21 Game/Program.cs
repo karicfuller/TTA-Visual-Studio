fcba867343ad3c0c1517d20f487acd91a9560c9c
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-18 16:54:37 -0800

    Exception Handling

--- 21 Game and Methods/21 Game/Program.cs
+++ 21 Game and Methods/21 Game/Program.cs
@@ -56,9 +56,18 @@ namespace _21_Game
 
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
-            
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank); //converts string to int
+                if (!validAnswer)
+                {
+                    Console.WriteLine("Please enter digits only no decimals.");
+                }
+            }
             
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
@@ -72,7 +81,28 @@ namespace _21_Game
 
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person out!");
+                        Console.ReadLine();
+                        return;
+                    }
+                    {
+                        Console.WriteLine("Something you entered was incorrect.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception) //general exception
+                    {
+                        Console.WriteLine("An error occurred. Please contact your system administrator.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
