73da448dae7f9e6bd4488e06f470d8fc98703f3a
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-19 21:12:50 -0800

    Incomplete

---
+++ TryCatch Assignment/TryCatch Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age.");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            string currentYear = DateTime.Now.Year.ToString();
+            string birthYear = 
+        }
+    }
+}
