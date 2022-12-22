bf670416781d876a8251928d86ce763afec2575d
Kari Fuller <kari.christine.fuller@gmail.com>
2022-12-21 21:26:40 -0800

    Added Database

--- 21 Game and Methods/21 Game/Program.cs
+++ 21 Game and Methods/21 Game/Program.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using Casino._21_Game;
+using Casino;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace _21_Game
 {
@@ -56,6 +59,20 @@ namespace _21_Game
 
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                foreach (var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + " | ");
+                    Console.Write(exception.ExceptionType + " | ");
+                    Console.Write(exception.ExceptionMessage + " | ");
+                    Console.Write(exception.TimeStamp + " | ");
+                    Console.WriteLine();
+                }
+                Console.Read();
+                return;
+            }
 
             bool validAnswer = false;
             int bank = 0;
@@ -85,20 +102,17 @@ namespace _21_Game
                     {
                         game.Play();
                     }
-                    catch (FraudException)
+                    catch (FraudException ex)
                     {
-                        Console.WriteLine("Security! Kick this person out!");
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithException(ex);
                         Console.ReadLine();
                         return;
                     }
-                    {
-                        Console.WriteLine("Something you entered was incorrect.");
-                        Console.ReadLine();
-                        return;
-                    }
-                    catch (Exception) //general exception
+                    catch (Exception ex) //general exception
                     {
                         Console.WriteLine("An error occurred. Please contact your system administrator.");
+                        UpdateDbWithException(ex);
                         Console.ReadLine();
                         return;
                     }
@@ -110,8 +124,66 @@ namespace _21_Game
             Console.WriteLine("Feel free to look around the casino. Bye for now.");
             Console.Read();
         }
+        private static void UpdateDbWithException(Exception ex)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TwentyOneGame;
+                                      Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                      TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                      MultiSubnetFailover=False";
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES
+                                (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TwentyOneGame;
+                                      Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                      TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                      MultiSubnetFailover=False";
+
+            string queryString = @"Select Id, ExceptionType, ExceptionMessage, TimeStamp From Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+            return Exceptions;
+        }
     }
 }
+//ADO.Net
 //inheritance is an is/a relationship, not a has/a relationship, then class property (if unsure, use property).
 //abstract class can never be an object. Only meant to be inherited from.
 //abstract method can only exist inside an abstract class/any class inheriting the abstract class, must inherit the method.
--- 21 Game and Methods/Casino/TwentyOneGame.cs
+++ 21 Game and Methods/Casino/TwentyOneGame.cs
@@ -40,7 +40,7 @@ namespace Casino._21_Game
                 }
                 if (bet < 0)
                 {
-                    throw new FraudException();
+                    throw new FraudException("Security! Kick this person out!");
                 }
                 bool successfullyBet = player.Bet(bet);
                 
